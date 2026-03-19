646337b6cc92e8a02f56ab9dae1adfab4983b134
ritvikbharti <bhartiritvik1000@gmail.com>
2026-03-19 23:18:47 +0530

    Iterator

---
+++ Design-Pattern/Behaivoural_pattern/Iterator.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Video{
+    String title;
+    public Video(String title){
+        this.title = title;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+}
+
+// class  YouTubePlaylist{
+//     private List<Video> videos = new ArrayList<>();
+
+//     public void addVideo(Video video){
+//         videos.add(video);
+//     }
+//     public List<Video> getVideos(){
+//         return videos;
+//     }
+
+// }
+
+interface PlaylistIterator{
+    boolean hasNext();
+    Video next();
+
+}
+
+// concrete iterrator- traversal algo 1
+class YouTubePlaylistIterator implements PlaylistIterator{
+    private List<Video> videos;
+    private int position;
+    public YouTubePlaylistIterator(List<Video> videos){
+        this.videos = videos;
+        this.position = 0;
+    }
+    @Override
+    public boolean hasNext(){
+        return position < videos.size();
+    }
+    @Override
+    public Video next(){
+        return hasNext() ? videos.get(position++) : null;
+    }
+}
+
+interface Playlist{
+    PlaylistIterator createIterator();
+}
+
+class YouTubePlaylist implements Playlist{
+    private List<Video> videos = new ArrayList<>();
+
+    public void addVideo(Video video){
+        videos.add(video);
+    }
+    @Override
+    public PlaylistIterator createIterator(){
+        return new YouTubePlaylistIterator(videos);
+    }
+}
+
+public class Iterator{
+    public static void main(String[] args) {
+        YouTubePlaylist playlist = new YouTubePlaylist();
+        playlist.addVideo(new Video("LLD TUTORIAL"));
+        playlist.addVideo(new Video("System Design Basics"));
+        YouTubePlaylist playlist2 = new YouTubePlaylist();
+        playlist2.addVideo(new Video("JAVA_SPRINGBOOT_TELLUSKO"));
+
+
+
+
+// ----------------before Iterartor
+              /*   for(Video v : playlist.getVideos()){
+                    System.out.println(v.getTitle());
+                }
+                */
+
+            PlaylistIterator iterator = playlist2.createIterator();
+            while(iterator.hasNext()){
+                System.out.println(iterator.next().getTitle());
+            }
+
+    }
+}
